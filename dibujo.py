9bf5587129e6eb6e138b17289338da0d8177d52c
Sergio Morales <s3naserg1o@gmail.com>
2025-08-25 17:02:58 -0500

    primer commit

---
+++ dibujo.py
@@ -0,0 +1,12 @@
+import turtle
+ventana = turtle.Screen()
+t= turtle.Turtle()
+t.shape("turtle")
+t.color("red")
+t.width(0.5)
+t.speed(100) 
+a=1
+while True:
+    t.fd(a)
+    t.lt(190)
+    a=a+1
